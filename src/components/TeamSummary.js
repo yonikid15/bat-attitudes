dbbd9c3d904dc8d492841cd90e347a57bbc3b2ab
Yonathan <yoni.kid15@gmail.com>
2018-07-03 22:04:31 -0400

    new score

--- src/components/TeamSummary.js
+++ src/components/TeamSummary.js
@@ -6,7 +6,7 @@ const TeamSummary = () => {
         <div className="page-header">
             <div className="content-container">
                 <div className="page-header__title">
-                    <h1>Wins: <span>2</span></h1>
+                    <h1>Wins: <span>3</span></h1>
                     <h1>Losses: <span>3</span></h1>
                 </div>
             </div>
