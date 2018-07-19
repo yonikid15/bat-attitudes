7c77fb59788b56b5a009f3db4e65bde2028578aa
Yonathan <yoni.kid15@gmail.com>
2018-07-19 18:31:54 -0400

    updated score and added standing

--- src/components/TeamSummary.js
+++ src/components/TeamSummary.js
@@ -6,8 +6,12 @@ const TeamSummary = () => {
         <div className="page-header">
             <div className="content-container">
                 <div className="page-header__title">
-                    <h1>Wins: <span>3</span></h1>
-                    <h1>Losses: <span>3</span></h1>
+                    <h1>Standing: <span>10th</span></h1>
+                </div>
+                <div className="page-header__title">
+                    <h1>Wins: <span>4</span></h1>
+                    <h1>Losses: <span>5</span></h1>
+                    <h1>Missed: <span>1</span></h1>
                 </div>
             </div>
         </div>
